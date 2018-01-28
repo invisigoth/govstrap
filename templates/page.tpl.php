0475f12cbae723519f375cdf0411fc3b3eb37911
Jason Guo <jason@xing.net.au>
2018-01-28 19:33:15 +1100

    adjusted homepage margins/paddings

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -14,33 +14,26 @@
 
 <main>
   <div id="main" class="main-container <?php print $container_class; ?>">
-    <?php if (!empty($page['highlighted'])): ?>
-      <div class="highlighted"><?php print render($page['highlighted']); ?></div>
-    <div id="parking-info">
-        <div class="row">
-            <div class="col-sm-4"><strong>Parking in the parlimentary zone</strong></div>
-            <div class="col-sm-8 text-right">
-                <span class="link"><a href="/parking/parking-areas"><i class="icon-parking"></i>Information about paid parking</a></span>
-                <span class="link"><a href="/parking/infringement-payment"><i class="icon-money"></i>Pay a parking infringement notice</a></span>
-            </div>
-        </div>
-    </div>
-    <?php endif; ?>
+      <div class="highlighted">
+          <?php if (!empty($page['highlighted'])): ?>
+              <?php print render($page['highlighted']); ?>
+          <?php endif; ?>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+              <h1 class="page-header"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+      </div>
+
     <?php print $messages; ?>
     <div id="content" class="row">
       <?php if (!empty($page['sidebar_first'])): ?>
-        <aside class="col-sm-3" role="complementary">
+        <aside class="col-sm-4" role="complementary">
           <?php print render($page['sidebar_first']); ?>
         </aside>  <!-- /#sidebar-first -->
       <?php endif; ?>
       <section id="main-content-section" class="<?php print $content_column_class; ?>" role="main">
         <div id="main-content"></div>
-        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-        <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-          <h1 class="page-header"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
         <?php endif; ?>
@@ -55,7 +48,7 @@
         </div>
       </section>
       <?php if (!empty($page['sidebar_second'])): ?>
-        <aside class="col-sm-3" role="complementary">
+        <aside class="col-sm-4" role="complementary">
           <?php print render($page['sidebar_second']); ?>
         </aside>  <!-- /#sidebar-second -->
       <?php endif; ?>
